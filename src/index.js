1a8f443edad7f3e7b15576822af35191f4fbaabf
William Pinto <william.pinto@lambda3.com.br>
2019-12-10 19:02:20 -0300

    Implemented full tutorial (added Time Travel capability and refactored

--- src/index.js
+++ src/index.js
@@ -38,43 +38,15 @@ function CalculateWinner(boardSquares){
 
 class Board extends React.Component {
 
-	constructor(){
-		super();
-		this.state = {
-			squaresValues: Array(9).fill(null),
-			xIsNext: true
-		}
-	}
-
-	defineNextPlayer(){
-		return this.state.xIsNext ? 'X' : 'O';
-	}
-
-	updateSquareValue(elementPosition) {
-		const newSquareList = this.state.squaresValues.slice();
-
-		if (CalculateWinner(newSquareList)) {
-      return;
-		}
-
-		newSquareList[elementPosition] = this.defineNextPlayer();
-		this.setState({ squaresValues: newSquareList, xIsNext: !this.state.xIsNext });
-	}
-
   renderSquare(elementPosition) {
 		return <Square
-			value={this.state.squaresValues[elementPosition]}
-			onClick={() => this.updateSquareValue(elementPosition)}
+			value={this.props.squaresValues[elementPosition]}
+			onClick={() => this.props.onClick(elementPosition)}
 		/>;
   }
-
   render() {
-		const winner = CalculateWinner(this.state.squaresValues);
-		const status = winner ? `Winner: ${winner}` : `Next player: ${(this.state.xIsNext ? 'X' : 'O')}`;
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -96,15 +68,71 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  render() {
+	constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squaresValues: Array(9).fill(null),
+      }],
+			stepNumber: 0,
+			xIsNext: true,
+    };
+	}
+
+	jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+	}
+
+	definePlayerMove(){
+		return this.state.xIsNext ? 'X' : 'O';
+	}
+
+	updateSquareValue(elementPosition) {
+		const gameHistory = this.state.history.slice(0, this.state.stepNumber + 1); //certifica que se nós “voltarmos no tempo”, e então fizermos uma nova jogada a partir daquele ponto, descartamos todo o histórico do “futuro” que agora se tornaria incorreto.
+		const currentGame = gameHistory[this.state.stepNumber];
+		const newSquareList = currentGame.squaresValues.slice();
+
+		if (CalculateWinner(newSquareList)) {
+      return;
+		}
+
+		newSquareList[elementPosition] = this.definePlayerMove();
+		this.setState({
+			history: gameHistory.concat([{ squaresValues: newSquareList }]),
+			stepNumber: gameHistory.length,
+			xIsNext: !this.state.xIsNext
+		});
+	}
+
+	render() {
+		const currentGame = this.state.history[this.state.history.length - 1];
+		const winner = CalculateWinner(currentGame.squaresValues);
+		const status = winner ? `Winner: ${winner}` : `Next player: ${(this.state.xIsNext ? 'X' : 'O')}`;
+		const gameMoves = this.state.history.map((step, move) => {
+			const desc = move ?
+			'Go to move #' + move :
+			'Go to game start';
+			return (
+					<li key={move}>
+						<button onClick={() => this.jumpTo(move)}>{desc}</button>
+					</li>
+				);
+		});
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+						squaresValues={currentGame.squaresValues}
+						onClick={(elementPosition) => this.updateSquareValue(elementPosition)}
+					/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{gameMoves}</ol>
         </div>
       </div>
     );
@@ -122,6 +150,11 @@ Square.propTypes = {
 	value: PropTypes.string,
 	onClick: PropTypes.func.isRequired
 };
+
+Board.propTypes = {
+	squaresValues: PropTypes.arrayOf(PropTypes.string),
+	onClick: PropTypes.func.isRequired
+}
 
 
 //https://pt-br.reactjs.org/docs/optimizing-performance.html#examples
